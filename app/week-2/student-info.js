e681d74865640dd01016d9868e977f859b77acbb
Dexter Balino <balino_dexter@yahoo.com>
2024-01-29 23:48:53 -0700

    initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const StudentInfo = ({ name, link }) => {
+    return (
+        <div>
+            <p>Dexter Balino</p>
+            <a href="https://github.com/ElysianCode12">My Github</a>
+        </div>
+    );
+};
