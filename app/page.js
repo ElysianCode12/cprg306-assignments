b5956513edae8f7428a7a8c0ea7750c79a8b470b
Dexter Balino <balino_dexter@yahoo.com>
2024-02-06 13:12:36 -0700

    added week 3

--- app/page.js
+++ app/page.js
@@ -4,8 +4,14 @@ export default function Page() {
 	return (
 		<main>
 			<h1>CPRG 306: Web Development 2 -Assignments</h1>
-			<Link href='./week-2'>week 2</Link>
-			<Link href='./week-3'>week 3</Link>
+			<ul>
+				<li>
+					<Link href='./week-2'>week 2</Link>
+				</li>
+				<li>
+					<Link href='./week-3'>week 3</Link>
+				</li>
+			</ul>
 		</main>
 	);
 }
